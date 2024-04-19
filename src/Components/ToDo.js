fa34695869d2939aeec8675b25b831ee10d21186
Houcine Hannoun <hanoun.houssin@gmail.com>
2024-04-19 11:02:42 +0100

    create todo form

--- src/Components/ToDo.js
+++ src/Components/ToDo.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
 
-function ToDo() {
+function ToDo({task}) {
   return (
-    <div>ToDo</div>
+    <div className='Todo'>
+      <p>{task.task}</p>
+      <div>
+        <FontAwesomeIcon icon={faPenToSquare} />
+        <FontAwesomeIcon icon={faTrash} />
+      </div>
+
+    </div>
   )
 }
 
--- src/Components/ToDoWrapper.js
+++ src/Components/ToDoWrapper.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
+import ToDoForm from './ToDoForm';
+import {v4 as uuidv4} from 'uuid';
+import ToDo from './ToDo';
+uuidv4();
 
 const ToDoWrapper = () => {
+
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    setTodos([...todos, {id: uuidv4(), task: todo, completed: false, isEditing: false}])
+  }
+
   return (
-    <div>ToDoWrapper</div>
+    <div className='TodoWrapper'>
+      <h1>Get Things Done!</h1>
+      <ToDoForm addTodo={addTodo} />
+      {todos.map((todo, index) => (
+        <ToDo task={todo} key={index} />
+      ))}
+    </div>
   )
 }
 
